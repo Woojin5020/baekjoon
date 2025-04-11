6f1985c4d657359caf450b4916a0e80338a26655
Woojin5020 <44234169+Woojin5020@users.noreply.github.com>
2025-04-11 14:03:24 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	scanf("%d", &num);
+
+	if ((num % 4 == 0 && num % 100 != 0) || (num % 400 == 0)) {
+			printf("%d", 1);
+	}
+	else
+	{
+		printf("%d", 0);
+	}
+	return 0;
+}
