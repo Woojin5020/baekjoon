1b80ffafa15ca29a3237f8c120a4ac8c7121c71c
Woojin5020 <44234169+Woojin5020@users.noreply.github.com>
2025-04-27 18:37:37 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char alpha_str[100];  // 문자열을 입력받을 배열
+	scanf("%s",alpha_str); // baekjoon
+
+	int count = 0;     
+	int value[26] = {0}; //결과가 들어갈 알파벳 배열
+	int value_index;
+
+	for (int i = 0; i < sizeof(value) / sizeof(int); i++) *(value + i) = -1; // 알파벳 a~z배열을 -1로 초기화
+
+	while(*(alpha_str + count) != '\0')
+	{
+		value_index = *(alpha_str + count) - 97;
+		if (*(value + value_index) == -1)
+		{
+			*(value + value_index) = count;
+			count++;
+		}
+		else if (*(value + value_index) != -1)
+		{
+			count++;
+		}
+		continue;
+	}
+
+	for (int i = 0; i < sizeof(value) / sizeof(int); i++) printf("%d ",*(value + i));
+	return 0;
+}
